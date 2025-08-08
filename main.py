5e0e9ee71f37492f48db99e09a70b688619a513a
Celestine27 <223501507+Celestine27@users.noreply.github.com>
2025-08-08 20:23:19 +0200

    stats.py character count

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_char
 
 def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_char = get_num_char(text)
     print(f"{num_words} words found in the document")
+    print(num_char)
+    
     
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -2,3 +2,20 @@ def get_num_words(text):
     """Splits the text into a list and counts the number of items in that list."""
     words = text.split()
     return len(words)
+
+def get_num_char(text):
+    char_value = {}
+    for c in text.lower():
+        if c not in char_value:
+            char_value[c] = 1
+        else:
+            char_value[c] += 1
+    return char_value
+
+        
+        
+
+    
+
+
+
